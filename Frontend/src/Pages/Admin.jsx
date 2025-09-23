e79aa26ee6d613498bd30b4063115de4c41920a1
fRankEnstein1-1-1-1 <frankensteinmenon@gmail.com>
2025-09-23 18:12:30 +0530

    trial 3

--- Frontend/src/Pages/Admin.jsx
+++ Frontend/src/Pages/Admin.jsx
@@ -6,7 +6,7 @@ import "./Admin.css"; // import styles
 
 export default function Admin() {
   // Change the URL to your localhost and port (e.g., http://localhost:4000)
-  const socket = io("gdgcbuzzer-production.up.railway.app", {
+  const socket = io("https://gdgcbuzzer-production.up.railway.app", {
     transports: ["websocket"],
     withCredentials: true,
   });
