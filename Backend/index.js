9b44175f373e214ac6fc69871476bed66536a5d8
fRankEnstein1-1-1-1 <frankensteinmenon@gmail.com>
2025-09-23 18:16:56 +0530

    trial 4

--- Backend/index.js
+++ Backend/index.js
@@ -44,5 +44,5 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
