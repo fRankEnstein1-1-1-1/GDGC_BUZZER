1e3df3307710747d00c5691244a2643f05af86c1
fRankEnstein1-1-1-1 <frankensteinmenon@gmail.com>
2025-09-23 17:34:18 +0530

    trial 0

--- Frontend/src/Pages/Admin.jsx
+++ Frontend/src/Pages/Admin.jsx
@@ -5,37 +5,38 @@ import { io } from "socket.io-client";
 import "./Admin.css"; // import styles
 
 export default function Admin() {
-  // Change the URL to your localhost and port (e.g., http://localhost:4000)
-  const socket = io("http://localhost:4000", {
-    transports: ["websocket"],
-    withCredentials:true
-});
+  // Change the URL to your localhost and port (e.g., http://localhost:4000)
+  const socket = io("http://localhost:4000", {
+    transports: ["websocket"],
+    withCredentials: true,
+  });
 
-  const [winner, setWinner] = useState("Waiting...");
+  const [winner, setWinner] = useState("Waiting...");
 
-  useEffect(() => {
-    socket.on("winner", (team) => setWinner(`Winner: ${team}`));
-    socket.on("reset", () => setWinner("Waiting..."));
-  }, []);
+  useEffect(() => {
+    socket.on("winner", (team) => setWinner(`Winner: ${team}`));
+    socket.on("reset", () => setWinner("Waiting..."));
+  }, []);
 
-  const reset = () => socket.emit("reset");
+  const reset = () => socket.emit("reset");
 
-  return (
-    <>
-      <div className="nav">
-        <div className="logo">
-          <img src="/gdgcl.jpg" alt="Logo" />
-        </div>
-        <div className="logotext">
-          <h3>Admin</h3>
-        </div>
-      </div>
-      <div className="admin-root">
-        <h1 className="admin-winner">{winner}</h1>
-        <button className="admin-reset-btn" onClick={reset}>
-          Reset
-        </button>
-      </div>
-    </>
-  );
+  return (
+    <>
+      <div className="nav">
+        <div className="logo">
+          <img src="/gdgcl.jpg" alt="Logo" />
+        </div>
+        <div className="logotext">
+          <h3>Admin</h3>
+        </div>
+      </div>
+      <div className="admin-root">
+        <h1 className="admin-winner">{winner}</h1>
+        <button className="admin-reset-btn" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </>
+  );
 }
+
